6a9867d62fd58b48b333a823fc30c735d9f9d267
dani miletic <danimiletic@gmail.com>
2023-06-07 12:25:30 -0600

    finsihed user login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import { AuthConsumer } from '../../providers/AuthProvider';
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
+  const [user, setUser] = useState({email: '', password: ''})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    handleLogin(user)
+    setUser({email: '', password: ''})
+  }
+
   return (
     <>
       <h1>Login Page</h1>
+      <form>
+        <label>Email</label>
+          <input 
+            type='email'
+            name='email'
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            required
+            autoFocus
+          />
+          <input 
+            type='password'
+            name='password'
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            required
+            autoFocus
+          />
+      </form>
+      <button type='submit'>Submit</button>
     </>
   )
 }
-
-export default Login;
+const ConnectedLogin = (props) => (
+  <AuthConsumer>
+    { value => <Login {...props} {...value} />}
+  </AuthConsumer>
+)
+export default ConnectedLogin;
